d32b90f75c72280bad4901c6d100437d9c1449f1
Lars Koreska Andersen <lkan12@student.aau.dk>
2018-02-20 11:34:49 +0100

    Network
    network

---
+++ VRConcert/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    string _room = "My_Room";
+    // Use this for initialization
+    void Start()
+    {
+
+        PhotonNetwork.ConnectUsingSettings("0.01");
+        if (PhotonNetwork.connected)
+        {
+            Debug.Log("Connected");
+        }
+
+    }
+    void OnJoinedLobby()
+    {
+        print("In Lobby");
+
+        RoomOptions roomOptions = new RoomOptions() { };
+        PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.playerList.Length);
+
+        if(PhotonNetwork.playerList.Length < 2)
+        {
+            PhotonNetwork.Instantiate("PlayerOne", new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+        }else if (PhotonNetwork.playerList.Length < 3)
+        {
+            PhotonNetwork.Instantiate("PlayerTwo", new Vector3(0f, 100f, 0f), Quaternion.identity, 0);
+        }
+    }
+
+}
---
+++ VRConcert/Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedPlayer : Photon.MonoBehaviour {
+
+    public Transform playerGlobal;
+    public Transform playerLocal;
+
+    PhotonView photonView;
+    private GameObject spawnedCameraRig;
+    // Use this for initialization
+    void Start () {
+
+        photonView = GetComponent<PhotonView>();
+
+        if (photonView.isMine)
+        {
+            spawnedCameraRig = (GameObject)Instantiate(Resources.Load("PlayerOne"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+            playerGlobal = GameObject.Find("PlayerOne(Clone)").transform;
+            playerLocal = playerGlobal.Find("[CameraRig]/Camera (head)/Camera (eye)");
+            if(playerLocal == null)
+            {
+                playerLocal = playerGlobal.Find("[CameraRig]/Camera (eye)");
+            }
+            this.transform.SetParent(playerLocal);
+            this.transform.localPosition = Vector3.zero;
+        }
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
